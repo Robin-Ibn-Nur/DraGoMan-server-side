41c4bb0e59e4aa3d1db99ea28da7c45021644d60
saifsammy <saifsammy653@gmail.com>
2022-10-28 18:04:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+const Port = process.env.PORT || 5000;
+
+
+const languageCollection = require('./data/courses.json')
+
+app.get('/', (req, res) => {
+    res.send('server runnig')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(languageCollection);
+    // console.log(languageCollection);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params?.id;
+    const selectedCourse = languageCollection?.find(c => c.id == id)
+    // console.log(selectedCourse);
+    res.send(selectedCourse)
+})
+
+
+
+app.listen(Port, () => {
+    console.log('server is running on port', Port)
+})
+
+module.exports = app;
